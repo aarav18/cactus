fc6290be034d49687c4b2cb8e4581894f4b6c03f
Aarav Shah <aaravshah18@gmail.com>
2026-02-13 02:30:46 -0800

    added M check to optimize low-dimensional matmul (no SME2)
    Signed-off-by: Aarav Shah <aaravshah18@gmail.com>

--- cactus/kernel/kernel_matmul.cpp
+++ cactus/kernel/kernel_matmul.cpp
@@ -151,6 +151,8 @@ static void cactus_matmul_f16_worker(
 }
 
 #if defined(CACTUS_COMPILE_SME2)
+constexpr size_t SME2_M_THRESHOLD = 4;
+
 void cactus_matmul_f16_sme2_caller(
 	const __fp16* a,
 	const __fp16* b_transposed,
@@ -171,7 +173,7 @@ void cactus_matmul_f16(
 ) {
 
 #if defined(CACTUS_COMPILE_SME2)
-	if (cpu_has_sme2()) {
+	if (cpu_has_sme2() && M >= SME2_M_THRESHOLD) {
 		cactus_matmul_f16_sme2_caller(
 			a, b_transposed, c,
 			M, K, N
